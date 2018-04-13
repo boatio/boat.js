0e4ad6b0630e106ed9ab6c85e4228966b4655973
boatio <37399578+boatio@users.noreply.github.com>
2018-04-13 21:31:29 +0900

    Update canvas().js

--- boat.js function/canvas().js
+++ boat.js function/canvas().js
@@ -1,13 +1,12 @@
 var xz = inner.Width - 30;<br>
 var zy = inner.Hidth - 30;<br>
 canvas(Width,Height); //(make canvas in html)<br>
-{<br>
+{
 
-}<br>
-# ex)
-<pre><code>canvas(xz,xy);
-canvas(300,400);
-</code>
+}
+// ex) canvas(xz,xy);
+// ex)canvas(300,400);
+
 
 
 
