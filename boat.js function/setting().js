d8c7eed0e38f3fe63a1c25d75815d06474f9a363
boatio <37399578+boatio@users.noreply.github.com>
2018-04-13 21:34:20 +0900

    Create setting().js

---
+++ boat.js function/setting().js
@@ -0,0 +1,13 @@
+function setting(){
+  //code 
+  //ex) canvas(100,100);
+}
+//in your code 
+
+
+//ex main code
+function setting(){
+canvas(600,600);
+console.log("canvas loading 1");
+}
+console.log("canvas loading 2");
